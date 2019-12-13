34aa6f102f04631d48434a4eafa5a7d0fe483b72
NoharaShin <hichaka.baken@gmail.com>
2019-12-13 06:14:39 +0100

    Added complexe shuffle mode scenario

--- test/run.py
+++ test/run.py
@@ -5,6 +5,11 @@ from secret_santa import SecretSanta
 if __name__ == '__main__':
     input_path = os.path.abspath(os.path.join('', 'test', 'input', 'participants.csv'))
     santa = SecretSanta(input_path)
-    for i in range(100):
-        givers, receivers = santa.shuffle()
-        print(givers, receivers)
+
+    santa_pairs = santa.shuffle(simple_mode=False)
+    givers = [santa_pair.Giver for santa_pair in santa_pairs]
+    receivers = [santa_pair.Receiver for santa_pair in santa_pairs]
+
+    print(santa_pairs)
+    print(givers)
+    print(receivers)
